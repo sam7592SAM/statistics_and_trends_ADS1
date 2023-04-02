9e40c14fbbaba190e45d0c4500976fc5161ac21a
sam7592SAM <87634326+sam7592SAM@users.noreply.github.com>
2023-04-03 00:19:53 +0100

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,6 +1,9 @@
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as sts
 
 
 def read_data(data, country, years):
@@ -10,46 +13,56 @@ def read_data(data, country, years):
     df2 = df1.T
     return df1, df2
 
-'''
-#def stat_func():
-    
-'''
-def plot(data, kind, title,x,y):
+
+def stat_function(data):
+    print(data.describe())
+    print(data.corr())
+
+
+def plot(data, kind, title, x, y):
     data.plot(kind=kind)
     plt.title(title)
     plt.xlabel(x)
     plt.ylabel(y)
     plt.legend(loc='upper right')
-    
-    
-country_list = [19, 35, 43, 46, 49, 76, 87, 115, 129, 257]
-year = [33, 43, 53, 63]
-'''
-country_names = ["Spain", "United States", "United Kingdom", "China", "India", 
-                "Brazil", "Congo, Dem. Rep.", "Australia", "Switzerland",
-                "Cambodia"]
-
-year = ['1990', '1995', '2000', '2005', '2010', '2015', '2020']
-'''
-
-country_names = ["Australia", "Brazil", "Switzerland", "China", 
-                 "Congo, Dem. Rep.", "Spain", "United Kingdom", "India",
-                 "Switzerland", "Cambodia"]
 
 
-#reading values by calling functions
-forest_1, forest_2 = read_data("API_AG.LND.FRST.K2_DS2_en_csv_v2_5346556.csv", 
+country_list = [19, 41, 83, 72, 257]
+year = [43, 48, 53, 58, 63]
+
+
+country_names = ["Australia", "Canada", "France", "Ecuador", "United States"]
+
+
+'''reading values by calling functions'''
+
+
+forest_1, forest_2 = read_data("API_AG.LND.FRST.K2_DS2_en_csv_v2_5346556.csv",
                                country_list, year)
-poptot_1, poptot_2 = read_data("API_SP.POP.TOTL_DS2_en_csv_v2_5350834.csv", 
+poptot_1, poptot_2 = read_data("API_SP.POP.TOTL_DS2_en_csv_v2_5350834.csv",
                                country_list, year)
-agrlnd_1, agrlnd_2 = read_data("API_AG.LND.AGRI.ZS_DS2_en_csv_v2_5349988.csv", 
+agrlnd_1, agrlnd_2 = read_data("API_AG.LND.AGRI.ZS_DS2_en_csv_v2_5349988.csv",
                                country_list, year)
-aralnd_1, aralnd_2 = read_data("API_AG.LND.ARBL.ZS_DS2_en_csv_v2_5346580.csv", 
+aralnd_1, aralnd_2 = read_data("API_AG.LND.ARBL.ZS_DS2_en_csv_v2_5346580.csv",
                                country_list, year)
 
-forest_2.columns=country_names
+forest_2.columns = country_names
+poptot_2.columns = country_names
+agrlnd_2.columns = country_names
+aralnd_2.columns = country_names
+
 #forest_2.to_csv("forest.csv")
+
+stat_function(forest_2)
+stat_function(aralnd_2)
+
+print("Skew: ", sts.skew(forest_2["Ecuador"]))
+print("Kurtosis: ", sts.kurtosis(forest_2["Ecuador"]))
+
+
 plot(forest_2, 'bar', 'Forest Area (sq.km)', 'Years', 'No(in millions)')
-#plot(poptot_2, 'bar', 'Population, Total', 'Years', 'No(in millions)')
-#plot(agrlnd_2, 'line', 'Population, Total', 'Years', 'No(in millions)')
-#plot(aralnd_2, 'line', 'Population, Total', 'Years', 'No(in millions)')
+plot(aralnd_2, 'bar', 'Arable Land (% of land area)', 'Years',
+     'No(in millions)')
+plot(poptot_2, 'line', 'Population, Total', 'Years', 'No(in millions)')
+plot(agrlnd_2, 'line', 'Agricultural Land (% of land area)', 'Years',
+     'No(in millions)')
